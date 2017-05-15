2cb01a29e5789f0a8d69cf65b112638d229e752c
Franco <frfranco@uci.edu>
2017-05-14 19:02:59 -0700

    starting stemcells plugin

---
+++ wp-content/plugins/stemcells/admin/partials/stemcells-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://www.mind.uci.edu/
+ * @since      1.0.0
+ *
+ * @package    Stemcells
+ * @subpackage Stemcells/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ wp-content/plugins/stemcells/includes/class-stemcells-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.mind.uci.edu/
+ * @since      1.0.0
+ *
+ * @package    Stemcells
+ * @subpackage Stemcells/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Stemcells
+ * @subpackage Stemcells/includes
+ * @author     Ken Francisco <frfranco@uci.edu>
+ */
+class Stemcells_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
