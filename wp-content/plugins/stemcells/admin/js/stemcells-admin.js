21d49097d760b92b60e1dde8a5976bf9cdc15d68
Franco <frfranco@uci.edu>
2017-06-20 10:57:44 -0700

    starting redcap handler

--- wp-content/plugins/stemcells/admin/js/stemcells-admin.js
+++ wp-content/plugins/stemcells/admin/js/stemcells-admin.js
@@ -45,6 +45,47 @@
 		});
 	});
 
+
+	function redcapCall() {
+		// send data to php file through ajax
+		// source=http://stackoverflow.com/questions/6616539/post-ajax-data-to-php-and-return-data
+		// $.ajax({
+		// 	curl: 'parse-csv.php',
+		// 	type: 'POST',
+
+		// 	// must stringify(data) because php does not understand
+		// 	// [Object, Object, Object]
+		// 	// it only understands
+		// 	// [{"Record ID":"92",...}, {},... {}]
+		// 	data: {data: JSON.stringify(data)},
+		// 	success: function(result){
+		// 		console.log('ajaxCall() success');
+
+		// 		// maybe redirect to shop or output any error messages ...
+		// 		console.log(result);
+		// 	}});
+		// data.action = 'csv_handler';
+
+		$.ajax({
+			// this hardcoded url will probably break
+			// url: '/wp-admin/admin-ajax.php',
+			url: 'http://' + window.location.host+'/stemcells/wp-admin/admin-ajax.php',
+			type: 'POST',
+
+			// must stringify(data) because php does not understand
+			// [Object, Object, Object]
+			// it only understands
+			// [{"Record ID":"92",...}, {},... {}]
+			data: {data: JSON.stringify(data), action : 'redcap_handler'},
+			success: function(result){
+				//console.log('ajaxCall() success');
+				$('#info').html('csv import complete');
+
+				// maybe redirect to shop or output any error messages ...
+				console.log(result);
+			}});
+	}
+
 	function ajaxCall(data) {
 		// send data to php file through ajax
 		// source=http://stackoverflow.com/questions/6616539/post-ajax-data-to-php-and-return-data
@@ -71,7 +112,7 @@
 		$.ajax({
 			// this hardcoded url will probably break
 			// url: '/wp-admin/admin-ajax.php',
-			url: 'http://'+window.location.host+'/stemcells/wp-admin/admin-ajax.php',
+			url: 'http://' + window.location.host+'/stemcells/wp-admin/admin-ajax.php',
 			type: 'POST',
 
 			// must stringify(data) because php does not understand
